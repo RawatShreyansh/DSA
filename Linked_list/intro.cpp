3942d2c9e8f0b2855abe64c968fece532fc55ed6
Shreyansh Rawat <rawat.shreyansh09@gmail.com>
2023-07-28 23:59:36 +0530

    Files added

--- Linked_list/intro.cpp
+++ Linked_list/intro.cpp
@@ -1,18 +1,41 @@
-// This is the first file in the DSA 
-// First data structure Linked_list 
+// This is the first file in the DSA
+// First data structure Linked_list
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <cstddef>
 
 using namespace std;
 
-class Node{
-    public:
-    int data;
-    Node *next;
+class Node {
+public:
+  int data;
+  Node *next;
 };
 
-int main()
-{
-    
-    return 0;
+int main() {
+  Node *head;
+  Node *one = NULL;
+  Node *two = NULL;
+  Node *three = NULL;
+
+  one = new Node();
+  two = new Node();
+  three = new Node();
+
+  one->data = 1;
+  two->data = 2;
+  three->data = 3;
+
+  one->next = two;
+  two->next = three;
+  three->next = NULL;
+
+  head = one;
+  while (head != NULL) {
+    cout << head->data;
+    head = head->next;
+    cout << " -> ";
+  }
+
+  return 0;
 }
